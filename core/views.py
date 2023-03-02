2649281b14e79da7da08df4038e3b413702bec01
spirostrakadas <111435322+spirostrakadas@users.noreply.github.com>
2023-03-02 12:29:31 +0200

    added upload

--- core/views.py
+++ core/views.py
@@ -8,7 +8,9 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='signin')
 def index(request):
-    return  render(request,'index.html')
+    user_object=User.objects.get(username=request.user.username)
+    user_profile=Profile.objects.get(user=user_object)
+    return  render(request,'index.html',{'user_profile':user_profile})
 
 def sign_up(request):
 
