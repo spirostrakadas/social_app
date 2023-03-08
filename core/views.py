34fcd5c7ef1e3a3ac9dbb464e5972d0578884457
spirostrakadas <111435322+spirostrakadas@users.noreply.github.com>
2023-03-08 17:37:34 +0200

    suggestion

--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from .models import Profile,Post,LikePost,FollowersCount
 from django.contrib.auth.decorators import login_required
 from itertools import chain
+import random
 # Create your views here.
 
 @login_required(login_url='signin')
@@ -27,7 +28,30 @@ def index(request):
 
     feed_list=list(chain(*feed))
 
-    return  render(request,'index.html',{'user_profile':user_profile,"post":post,'posts':posts,'feed_list':feed_list})
+    #USER SUGGESTION START!
+    all_users=User.objects.all()
+    user_following_all=[]
+
+    for user in user_following:
+        user_list=User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+    new_suggestion_list=[x for x in list(all_users) if(x not in list(user_following_all))]
+    current_user=User.objects.filter(username=request.user.username)
+    final_suggestion_list=[x for x in list(new_suggestion_list) if(x not in list(current_user))]
+    random.shuffle(final_suggestion_list)
+
+    username_profile=[]
+    username_profile_list=[]
+
+    for users in final_suggestion_list:
+        username_profile.append(users.id)
+    for ids in username_profile:
+        profile_lists=Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+    suggestions_username_profile_lists=list(chain(*user_following_list))
+
+
+    return  render(request,'index.html',{'user_profile':user_profile,"post":post,'posts':posts,'feed_list':feed_list,"suggestions_username_profile_lists":suggestions_username_profile_lists})
 
 
 def search(request):
@@ -42,7 +66,7 @@ def search(request):
 
         for users in username_object:
             username_profile.append(users.id)
-            
+
         for ids in username_profile:
             profile_lists=Profile.objects.filter(id_user=ids)
             username_profile_list.append(profile_lists)
